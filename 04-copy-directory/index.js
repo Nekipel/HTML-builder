d08c6de35cb383aaca128ad6db4ae5e7c2974edf
Nekipel <88710417+Nekipel@users.noreply.github.com>
2022-05-23 17:01:33 +0300

    feat:add 4 part of task 04-copy-directory (Mon, May 23, 2022 05:01:33 PM)

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,23 @@
+const path = require('path');
+const { readdir, copyFile, rm, mkdir } = require('fs/promises');
 
+const dirOriginal = path.join(__dirname, 'files');
+const dirCopyCatalog = path.join(__dirname, 'files-copy');
+
+async function copyDir(dir, dirCopy) {
+  const dirFiles = await readdir(dir, { withFileTypes: true });
+  dirFiles.forEach(async function (element) {
+    if (element.isFile()) {
+      copyFile(path.join(dir, element.name), path.join(dirCopy, element.name));
+    } else if (element.isDirectory()) {
+      await mkdir(dirCopy + "\\" + element.name);
+      await copyDir(dir + "\\" + element.name, dirCopy + "\\" + element.name);
+    }
+  });
+}
+
+(async function () {
+  await rm(dirCopyCatalog, { recursive: true, force: true });
+  await mkdir(dirCopyCatalog);
+  await copyDir(dirOriginal, dirCopyCatalog);
+})();
