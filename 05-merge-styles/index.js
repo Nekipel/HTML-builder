f95f7fefbf8ecc1d7903f7c9f48d8d511e67c1ea
Nekipel <88710417+Nekipel@users.noreply.github.com>
2022-05-23 21:27:25 +0300

    feat:add 5 part of task 05-merge-styles(Mon, May 23, 2022 09:27:25 PM)

---
+++ 05-merge-styles/index.js
@@ -0,0 +1,28 @@
+const fs = require('fs');
+const path = require('path');
+const stylesFolder = path.join(__dirname, 'styles');
+
+async function createBundle(pathToFolderDev) {
+
+  const files = await fs.promises.readdir(pathToFolderDev, {
+    withFileTypes: true,
+  });
+
+  const content = fs.createWriteStream(
+    path.join(__dirname, "project-dist", "bundle.css"),
+    "utf-8"
+  );
+  
+  files.forEach((file) => {
+    let pathToFile = path.join(pathToFolderDev, file.name);
+    if (file.isFile() && path.extname(file.name) === ".css") {
+      const arr = [];
+      const readFiles = fs.createReadStream(pathToFile, "utf-8");
+      readFiles.on("data", (chunk) => arr.push(chunk));
+      readFiles.on("end", () => arr.forEach((x) => content.write(x + "\n")));
+      readFiles.on("error", (err) => console.log("Error: ", err.message));
+    }
+  });
+}
+
+createBundle(stylesFolder);
